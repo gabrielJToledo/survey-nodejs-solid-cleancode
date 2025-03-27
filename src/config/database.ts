7bcb40c137d1414a4a92bbc988bb14d64d149db5
GabrielJToledoUV <155473754+GabrielJToledoUV@users.noreply.github.com>
2025-03-27 05:13:18 -0300

    [FEAT] configure mongoDB connection setup

---
+++ src/config/database.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://mongo:27017/survey-db', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log('Mongo connected')
+    } catch (err) {
+        console.log('err: 'err) 
+        process.exit(1)
+    }
+}
